e670cd6c71df00a1417834639531fbee26b555d9
Wai Yan Aung <111115845+waiyanaung98@users.noreply.github.com>
2023-03-08 19:55:59 +0630

    Update App.js

--- functional components/App.js
+++ functional components/App.js
@@ -5,9 +5,9 @@ import Users from "./users";
 const App = (props) =>{
   return (
     <ul>
-      <Users name='aungaung' age='22' > </Users> 
+      <Users name='aungaung' age='22' /> 
       {/* //ဒီname and age က props ကိုဖြတ်ပြီး ဟိုဘက်မှာသွားပေါ်တာ */}
-      <Users name='maung maung' age='24' > </Users>
+      <Users name='maung maung' age='24' /> 
     </ul>
   )
 }
